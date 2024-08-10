739b4fd1a27f73e7e9b37308a5019c14b584d385
Anzor <anzor.pozner@gmail.com>
2024-08-10 12:58:34 +0100

    Mobile Nav link debugged. Added Restaurant management

--- src/components/MobileNavLinks.tsx
+++ src/components/MobileNavLinks.tsx
@@ -7,6 +7,13 @@ const MobileNavLinks = () => {
   const { logout } = useAuth0();
   return (
     <>
+        <Link
+        to="/manage-restaurant"
+        className="flex bg-white items-center font-bold hover:text-orange-500"
+      >
+        My Restaurant
+        </Link>
+
         <Link 
             to="/user-profile"
             className="flex bg-white items-center font-bold hover:text-orange-500"
